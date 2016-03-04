921a7401b6f2bf9c7e41891403c40329ee9740a0
Paul Chubinskiy <pchubinskiy@gmail.com>
2016-03-03 21:37:12 -0600

    styled artist info content; added label to x-axis; finished about page; got to MVP

---
+++ public/javascripts/index_d3.js
@@ -0,0 +1,36 @@
+  var data = JSON.parse(localStorage.getItem("listenersArray"));
+
+  d3.select("#graph")
+    .selectAll("div")
+      .data(data)
+    .enter().append("div")
+      // subtract 1/7 of the listeners amount from itself to match the
+      // domain:range ratio (7000000:600)
+      .style("width", function(d) { return ((d[1]/10000) - ((d[1]/10000)/7)) + "px" })
+      .html(function(d) { return d[0] })
+      .attr('class', 'bar')
+      .attr('id', function(d) { return d[0] })
+      .attr('href', function(d) { return d[3] });
+
+  var x = d3.scale.linear()
+                  .domain([0, 7000000])
+                  .range([0, 600]);
+
+  var xAxis = d3.svg.axis()
+                  .scale(x)
+                  .orient("bottom")
+                  .ticks(10);
+
+  var svg = d3.select("#graph").append("svg")
+      .attr("width", 900)
+      .attr("height", 40)
+
+  svg.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(10, 0)")
+      .call(xAxis)
+    .append("text")
+      .attr("x", 600)
+      .attr("dy", "2.71em")
+      .style("text-anchor", "end")
+      .text("Listeners");
--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,4 +1,9 @@
 $(function() {
+  $('#information').removeClass('hidden');
+  $('#artist_moused_over').html("Daft Punk");
+  $('#artist_image').attr('src', "http://img2-ak.lst.fm/i/u/174s/9fd718d4c80e4ac89107c6c0bb908440.png");
+  $('#genre_tags').html("Genre Tags:");
+  showGenre("electronic", 0); showGenre("dance", 1); showGenre("House", 2); showGenre("electronica", 3); showGenre("techno", 4);
 
   var apiMethod = '?method=chart.getTopArtists';
   var apiKey = '&api_key=b1edaebaa4cf27aec0976a586ff47224';
@@ -80,8 +85,10 @@ $(function() {
     });
 
     function passForInfoShow(response, item_id, link_for_image) {
+      $('#information').removeClass('hidden');
       $('#artist_moused_over').html(item_id);
       $('#artist_image').attr('src', link_for_image);
+      $('#genre_tags').html("Genre Tags:");
 
       for (var i = 0; i < 5; i++) {
         showGenre(response.toptags.tag[i].name, i);
@@ -92,5 +99,24 @@ $(function() {
       $('#genre_' + i).html(item);
     }
   });
+
+  $('.jumbotron').click(function(event) {
+    passForInfoShow("", "", "");
+  });
+
+  function passForInfoShow(response, item_id, link_for_image) {
+    $("#information").addClass('hidden');
+    $('#artist_moused_over').html(item_id);
+    $('#artist_image').attr('src', link_for_image);
+    $('#genre_tags').html("");
+
+    for (var i = 0; i < 5; i++) {
+      showGenre("", i);
+    }
+  }
+
+  function showGenre(item, i) {
+    $('#genre_' + i).html(item);
+  }
 
 });
